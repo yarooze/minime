d2e40090dd12f0ee355fe1ec01b8dd010fa82f32
jb <jn@sntt.de>
2015-06-02 13:52:29 +0200

    cache & headers

--- src/App/Core/MinimeHttpCache.php
+++ src/App/Core/MinimeHttpCache.php
@@ -1,6 +1,22 @@
 <?php
 namespace App\Core;
 
+if (!function_exists('getallheaders'))
+{
+  function getallheaders()
+  {
+    $headers = '';
+    foreach ($_SERVER as $name => $value)
+    {
+      if (substr($name, 0, 5) == 'HTTP_')
+      {
+        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+      }
+    }
+    return $headers;
+  }
+}
+
 /**
  *
  * @author Jasper  http://stackoverflow.com/questions/1971721/how-to-use-http-cache-headers-with-php
