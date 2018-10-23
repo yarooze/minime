74becaff1895bf0c2baadac9172e0e13cc503a1a
yarooze <yarooze@yahoo.de>
2018-10-23 15:55:24 +0200

    assets

--- skeleton/src/App/Templates/Main.tpl.php
+++ skeleton/src/App/Templates/Main.tpl.php
@@ -3,8 +3,12 @@
 <head>
     <meta charset="<?php echo $app->config->get('charset');?>">
     <title><?php if ($page_name !== null) { $view->printString($page_name); } else { echo "Minime"; } ?></title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+    <script src="/assets/js/jquery-3.3.1.min.js"></script>
+    <script src="/assets/js/popper.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
 </head>
 <body>
 <?php $view->renderPartial('headerPartial', array()); ?>
