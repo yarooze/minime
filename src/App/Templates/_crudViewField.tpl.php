d925d46e3e1e4658179a2b897a7517859df0a830
yarooze <yarooze@yahoo.de>
2018-10-25 14:13:02 +0200

    fix

--- src/App/Templates/_crudViewField.tpl.php
+++ src/App/Templates/_crudViewField.tpl.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__.'/../Helper/HtmlHelper.php';
-$title = (isset($entity['title'])) ? $i18n->trans($entity['title']) : $fieldName;
+$title = (isset($fieldData['title'])) ? $i18n->trans($fieldData['title']) : $fieldName;
 ?><div class="form-group row">
         <label for="<?php $view->printString($fieldName); ?>" class="col-sm-2 col-form-label"><?php $view->printString($title); ?></label>
 <div class="col-sm-10">
