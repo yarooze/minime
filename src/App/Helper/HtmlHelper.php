fbaeb2a6411d1f1dd39ea4ce624ce7c77488dd1c
_tsu <tsu@iichantra.ru>
2019-04-29 22:04:10 +0200

    multiselect

--- src/App/Helper/HtmlHelper.php
+++ src/App/Helper/HtmlHelper.php
@@ -18,7 +18,12 @@ if(!function_exists('make_select_options')) {
         echo '></option>';
         foreach ($data as $row) {
             echo '<option value="' . $row[$fieldId] . '" ';
-            echo ($value == $row[$fieldId]) ? 'selected="selected"' : '';
+            if (is_array($value)) {
+                echo (in_array($row[$fieldId], $value)) ? 'selected="selected"' : '';
+            } else {
+                echo ($value == $row[$fieldId]) ? 'selected="selected"' : '';
+            }
+            
             echo ' >' . $row[$fieldValue] . '</option>';
         }
     }
