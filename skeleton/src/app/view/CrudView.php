4f5f17b6c891884fe45cfadac881a9b76ec55993
_tsu <tsu@iichantra.ru>
2019-05-11 21:25:23 +0200

    Lost files

---
+++ skeleton/src/app/view/CrudView.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\View;
+
+/**
+ *
+ * @author jb
+ */
+Class CrudView extends MinimeCrudView
+{
+}
---
+++ skeleton/src/app/view/HtmlView.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\View;
+
+/**
+ *
+ * @author jb
+ */
+Class HtmlView extends MinimeHtmlView
+{
+}
