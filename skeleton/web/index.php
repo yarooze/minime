000ada9e84f668c13de3ed5d10ce21468069efa0
Jaroslav Bleicher <j.bleicher@circit.de>
2025-08-20 12:07:35 +0200

    v4.1.5 - Exceptions on DEV fix

--- skeleton/web/index.php
+++ skeleton/web/index.php
@@ -1,11 +1,13 @@
 <?php
 try {
     $app = require __DIR__ . '/../src/bootstrap.php';
-    $app->run(); 
-} catch (Exception $e) {
+    $app->run();
+} catch (\Throwable $e) {
     $app->logger->log(array(date('Y.m.d H:i:s'), $e->getMessage(), $e));
     if ($app->config->get('env') == 'dev') {
-        throw $e;
+        echo '<pre>';
+        print_r($e);
+        echo '</pre>';
     } else {
         echo 'sorry \(T_T)/';
         //echo $e->getMessage();
